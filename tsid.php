60cf3530e190c4a048ac46976c10f16e1a795901
antikirra <antikirra@gmail.com>
2024-01-30 05:55:13 +0700

    release v2

---
+++ tsid.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Antikirra;
+
+use InvalidArgumentException;
+
+/**
+ * @return int
+ */
+function tsid()
+{
+    list($tsid) = tsids(1);
+    return $tsid;
+}
+
+/**
+ * @param int $count
+ * @return int[]
+ */
+function tsids($count)
+{
+    static $prev;
+
+    if (!is_int($count) || $count < 1) {
+        throw new InvalidArgumentException();
+    }
+
+    $tsids = [];
+    for ($i = 0; $i < $count; $i++) {
+        while (true) {
+            $curr = (int)(microtime(true) * 1e9);
+            if ($curr > $prev) {
+                break;
+            }
+        }
+        $prev = $curr;
+        $tsids[] = $curr;
+    }
+
+    return $tsids;
+}
