82e0842e34a83120127f9b5cce592ede32332f77
antikirra <antikirra@gmail.com>
2024-02-12 20:45:22 +0700

    FIX

--- tsid.php
+++ tsid.php
@@ -4,38 +4,42 @@ namespace Antikirra;
 
 use InvalidArgumentException;
 
-/**
- * @return int
- */
-function tsid()
-{
-    list($tsid) = tsids(1);
-    return $tsid;
+if (!function_exists('Antikirra\tsid')) {
+    /**
+     * @return int
+     */
+    function tsid()
+    {
+        list($tsid) = tsids(1);
+        return $tsid;
+    }
 }
 
-/**
- * @param int $count
- * @return int[]
- */
-function tsids($count)
-{
-    static $prev;
+if (!function_exists('Antikirra\tsids')) {
+    /**
+     * @param int $count
+     * @return int[]
+     */
+    function tsids($count)
+    {
+        static $prev;
 
-    if (!is_int($count) || $count < 1) {
-        throw new InvalidArgumentException();
-    }
-
-    $tsids = [];
-    for ($i = 0; $i < $count; $i++) {
-        while (true) {
-            $curr = (int)(microtime(true) * 1e9);
-            if ($curr > $prev) {
-                break;
-            }
+        if (!is_int($count) || $count < 1) {
+            throw new InvalidArgumentException();
         }
-        $prev = $curr;
-        $tsids[] = $curr;
-    }
 
-    return $tsids;
+        $tsids = [];
+        for ($i = 0; $i < $count; $i++) {
+            while (true) {
+                $curr = (int)(microtime(true) * 1e9);
+                if ($curr > $prev) {
+                    break;
+                }
+            }
+            $prev = $curr;
+            $tsids[] = $curr;
+        }
+
+        return $tsids;
+    }
 }
