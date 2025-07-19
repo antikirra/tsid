a0a9a8c9690ca7b411c44e176100ac692b303485
antikirra <antikirra@gmail.com>
2025-07-19 14:04:35 +0700

    chore: remove obsolete function_exists checks

--- tsid.php
+++ tsid.php
@@ -4,40 +4,37 @@ namespace Antikirra;
 
 use InvalidArgumentException;
 
-if (!function_exists('Antikirra\\tsid')) {
-    /**
-     * @return int
-     */
-    function tsid()
-    {
-        list($tsid) = tsids(1);
-        return $tsid;
-    }
+/**
+ * @return int
+ */
+function tsid()
+{
+    list($tsid) = tsids(1);
+    return $tsid;
 }
 
-if (!function_exists('Antikirra\\tsids')) {
-    /**
-     * @param int $count
-     * @return int[]
-     */
-    function tsids($count)
-    {
-        static $offset = 0;
+/**
+ * @param int $count
+ * @return int[]
+ */
+function tsids($count)
+{
+    static $offset = 0;
 
-        if (!is_int($count) || $count < 1) {
-            throw new InvalidArgumentException(
-                sprintf('Expected positive integer for count, got %s', gettype($count))
-            );
-        }
-
-        $tsids = [];
-
-        for ($i = 0; $i < $count; $i++) {
-            $tsids[] = (int)(microtime(true) * 1e9) + (++$offset);
-        }
-
-        $offset = 0;
-
-        return $tsids;
+    if (!is_int($count) || $count < 1) {
+        throw new InvalidArgumentException(
+            sprintf('Expected positive integer for count, got %s', gettype($count))
+        );
     }
+
+    $tsids = [];
+
+    for ($i = 0; $i < $count; $i++) {
+        $tsids[] = (int)(microtime(true) * 1e9) + (++$offset);
+    }
+
+    $offset = 0;
+
+    return $tsids;
 }
+
